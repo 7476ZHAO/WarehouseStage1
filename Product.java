06a623eb0a0380fe27684b6326b94cca76298b21
7476ZHAO <7476zhao@gmail.com>
2025-10-22 01:34:07 -0500

    Update Product.java
    added processWaitlist()

--- Product.java
+++ Product.java
@@ -55,11 +55,18 @@ public class Product {
         waitlist.addItem(clientId, this.id, quantity);
     }
 
+    public void processWaitlist(int qty) {
+        int remaining = getWaitlist().fulfillItems(qty);
+        setStock(getStock() + remaining);
+    }
+
+
     // Print product info
     @Override
     public String toString() {
         return "Product[ID=" + id + ", Name=" + name + ", Price=" + price + ", Stock=" + stock + "]";
     }
 }
+
 
 
