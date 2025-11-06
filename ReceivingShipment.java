044d0617f8858a1b8131a9c7e53d31f513d386d1
7476ZHAO <7476zhao@gmail.com>
2025-11-06 02:02:17 -0600

    Update ReceivingShipment.java
    delete class receivingShipment

--- ReceivingShipment.java
+++ ReceivingShipment.java
@@ -1,43 +1,43 @@
-public class ReceivingShipment {
-    private ProductList productList;
+// public class ReceivingShipment {
+//     private ProductList productList;
 
-    public ReceivingShipment(ProductList productList) {
-        this.productList = productList;
-    }
+//     public ReceivingShipment(ProductList productList) {
+//         this.productList = productList;
+//     }
 
-    // Receive shipment by product name (case-insensitive)
-    public void receiveShipment(String productName, int qty) {
-        Product product = findProductByName(productName);
-        if (product == null) {
-            System.out.println("Sorry! Product not found: " + productName);
-            return;
-        }
+//     // Receive shipment by product name (case-insensitive)
+//     public void receiveShipment(String productName, int qty) {
+//         Product product = findProductByName(productName);
+//         if (product == null) {
+//             System.out.println("Sorry! Product not found: " + productName);
+//             return;
+//         }
 
-        double oldStock = product.getStock();
-        //product.setStock(oldStock + qty); ----already included in processWaitlist
+//         double oldStock = product.getStock();
+//         //product.setStock(oldStock + qty); ----already included in processWaitlist
 
-        System.out.println("Shipment received for " + productName);
-        System.out.println("Old stock: " + oldStock);
+//         System.out.println("Shipment received for " + productName);
+//         System.out.println("Old stock: " + oldStock);
 
-        // calling the processWaitlist function for later implementation of waitlist
-        product.processWaitlist(qty);
+//         // calling the processWaitlist function for later implementation of waitlist
+//         product.processWaitlist(qty);
 
-        System.out.println("New stock: " + product.getStock());
-    }
+//         System.out.println("New stock: " + product.getStock());
+//     }
 
-    // Helper method to find product by name
-    private Product findProductByName(String name) {
-        var iterator = productList.getProducts();
-        while (iterator.hasNext()) {
-            Product p = iterator.next();
-            if (p.getName().equalsIgnoreCase(name)) {
-                return p;
-            }
-        }
-        return null;
-    }
+//     // Helper method to find product by name
+//     private Product findProductByName(String name) {
+//         var iterator = productList.getProducts();
+//         while (iterator.hasNext()) {
+//             Product p = iterator.next();
+//             if (p.getName().equalsIgnoreCase(name)) {
+//                 return p;
+//             }
+//         }
+//         return null;
+//     }
 
-    public void printAllProducts() {
-        productList.printProduct();
-    }
-}
+//     public void printAllProducts() {
+//         productList.printProduct();
+//     }
+// }
